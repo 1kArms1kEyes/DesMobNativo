891b77242becff9bf9a1773509f08a45370110e8
MariaPaulaBN <mpaulabn1@gmail.com>
2025-11-09 04:21:12 -0500

    Se realizó el funcionamiento para la creación de los productos en la base de datos.

--- app/src/main/java/com/example/appmobile/activities/ListaDeProductoActivity.kt
+++ app/src/main/java/com/example/appmobile/activities/ListaDeProductoActivity.kt
@@ -1,16 +1,27 @@
 package com.example.appmobile.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.appmobile.R
 
+import com.example.appmobile.activities.CreacionDeProductosActivity
+
 class ListaDeProductoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_lista_de_producto)
+
+        val btnAdd = findViewById<ImageView>(R.id.btnAdd)
+
+        btnAdd.setOnClickListener {
+            val intent = Intent(this, CreacionDeProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
